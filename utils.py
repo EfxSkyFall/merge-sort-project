ad8bb1dcbb3284fa04fb0d58c180b196d2171649
EfxSkyFall <19wrobbens@salesian.surrey.sch.uk>
2025-01-21 13:49:58 +0000

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 ## utils
 import random
 def rdm_list():
-       size = int(input("How many values do you want in this list ?")
+       size = int(input("How many values do you want in this list ?"))
        lowb = int(input("Enter the lower bound of the list : "))
        upb = int(input("Enter the upper bound of the list : "))
        lst = []
